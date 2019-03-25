8468b099853b1914cea4053f5f6b5caf22966999
Basheer Khlaif <Basheerkhlaif@gmail.com>
2019-03-26 01:36:03 +0200

    first commit

---
+++ src/main/java/com/bafapp/BaFappApplication.java
@@ -0,0 +1,13 @@
+package com.bafapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaFappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaFappApplication.class, args);
+	}
+
+}
